18abb630d3ce204e0e05cab3acd67f711237d87b
Dominic Steinitz <dominic@steinitz.org>
2013-11-15 08:04:37 +0000

    Chris' latest changes.

--- RedBlackStencil.hs
+++ RedBlackStencil.hs
@@ -17,10 +17,10 @@ solveLaplace
     -> Double		-- ^ weight for over relaxing (>0.0 and <2.0).     -- new
 	-> Array U DIM2 Double	-- ^ Boundary value mask.
 	-> Array U DIM2 Double	-- ^ Boundary values.
-	-> Array U DIM2 Double	-- ^ Initial state.
+	-> Array U DIM2 Double	-- ^ Initial state. Should have even number of columns
 	-> m (Array U DIM2 Double)
 
-solveLaplace !steps !omega !arrBoundMask !arrBoundValue !arrInit       -- new arg
+solveLaplace !steps !omega !arrBoundMask !arrBoundValue !arrInit       -- new
  =  do
      rBM   <- computeP $ extractRed arrBoundMask
      bBM   <- computeP $ extractBlack arrBoundMask
@@ -32,14 +32,14 @@ solveLaplace !steps !omega !arrBoundMask !arrBoundValue !arrInit       -- new ar
                                   (\ _ (e :. i :. j) -> even i) 
                         -- bool array of shape rInit indicating even rows    
      let relaxL r b = relaxLaplace omega r b rBM bBM rBV bBV evenRows
-         go 0 !r !b = computeP $ combineRB r b
-         go n !r !b 
+         go 0 r !b = computeP $ combineRB r b
+         go n r !b 
            = do 
                (newR,newB) <- relaxL r b
                go (n - 1) newR newB
      go steps rInit bInit
 
-relaxLaplace
+relaxLaplace                                                        -- new
 	:: Monad m
     => Double
     -> Array U DIM2 Double	
@@ -50,7 +50,7 @@ relaxLaplace
 	-> Array U DIM2 Double	
     -> Array U DIM2 Bool	
 	-> m (Array U DIM2 Double, Array U DIM2 Double)
-relaxLaplace omega r b rBM bBM rBV bBV evenRows
+relaxLaplace !omega r !b !rBM !bBM !rBV !bBV !evenRows
          = do
              r' <- computeP
                    $ A.szipWith (+) rBV
@@ -64,21 +64,22 @@ relaxLaplace omega r b rBM bBM rBV bBV evenRows
                    $ A.smap (/4)
                    $ altMapStencil2 (BoundConst 0) rightSt leftSt r' evenRows
                    -- Note use of r' rather than r to compute b'
-    --    return (r', b') will work but ignores over relaxation using omega
-             r'' <- computeP
+
+    {-   This computation of r'' does not seem to add much as it is not used in 
+         future iterations if returned with b'' until the last step
+              r'' <- computeP
                        $ A.zipWith (+) (A.map (* (1- omega)) r)
-                                        (A.map (* omega) (r'::Array U DIM2 Double))
+                                        (A.map (* omega) (r'::Array U DIM2 Double)) -}
              b'' <- computeP
                        $ A.zipWith (+) (A.map (* (1- omega)) b)
-                                        (A.map (* omega) b')
-             return (r'' , b'')
-     --      
+                                        (A.map (* omega) b') 
+             return (r' , b'')      
 
 combineRB :: Array U DIM2 Double -> Array U DIM2 Double -> Array D DIM2 Double
 combineRB r b =     -- arr(i,j) 
                     --     | even(i+j) = r(i, j `div` 2)
                     --     | otherwise = b(i, j `div` 2)
-                    -- arr has i <- 0..2n-1 , j <- 0..2m-1
+                    -- arr has i <- 0..n-1 , j <- 0..2m-1
                     -- r   has i <- 0..n-1 , j <- 0..m-1
                     -- b   has i <- 0..n-1 , j <- 0..m-1
             traverse2 r b
@@ -88,39 +89,43 @@ combineRB r b =     -- arr(i,j)
                                 (e :. i :. j `div` 2))
 
 extractRed :: Array U DIM2 Double -> Array D DIM2 Double
-extractRed arr =     -- r(i,j) = arr(i, 2*j - (i `mod` 2))
-                     -- arr has i <- 0..2n-1, j <- 0..2m-1
+extractRed arr =  
+    -- Expects even number of columns for arr (one added before div for odd case)
+                     -- r(i,j) = arr(i, 2*j - (i `mod` 2))
+                     -- arr has i <- 0..n-1, j <- 0..2m-1
                      -- r   has i <- 0..n-1 , j <- 0..m-1
               traverse arr 
-                      (\ (e :. i :. j) -> (e :. i :. (j `div` 2)))
+                      (\ (e :. i :. j) -> (e :. i :. ((j+1) `div` 2)))
                       (\get (e :. i :. j) -> get (e :. i :. 2*j + (i `mod` 2)))
 
 extractBlack :: Array U DIM2 Double -> Array D DIM2 Double
-extractBlack arr =   -- b(i,j) = arr(i, 2*j - ((i+1) `mod` 2))
-                     -- arr has i <- 0..2n-1, j <- 0..2m-1
+extractBlack arr =  
+    -- Expects even number of columns for arr (one added before div for odd case)
+                     -- b(i,j) = arr(i, 2*j - ((i+1) `mod` 2))
+                     -- arr has i <- 0..n-1, j <- 0..2m-1
                      -- b   has i <- 0..n-1 , j <- 0..m-1
              traverse arr 
-                      (\ (e :. i :. j) -> (e :. i :. (j `div` 2)))
+                      (\ (e :. i :. j) -> (e :. i :. ((j+1) `div` 2)))
                       (\get (e :. i :. j) -> get (e :. i :. 2*j + ((i+1) `mod` 2)))
 
 
-{- altMapStencil2
-  :: (Source r1 c, Source r2 Bool) =>
-     Boundary c
-     -> Stencil DIM2 c
-     -> Stencil DIM2 c
-     -> Array r1 DIM2 c
-     -> Array r2 DIM2 Bool
-     -> Array D DIM2 c 
--}                              
-altMapStencil2 bd s1 s2 x bools
-        -- maps stencils s1 or s2 indicated by bools array
-        -- currently does both on all items and selects after
-         = alt (mapStencil2 bd s1 x) (mapStencil2 bd s2 x) bools
-           where alt l r bools
-                   = A.zipWith selector (A.zipWith (,) l r) bools
+altMapStencil2
+  :: Boundary Double
+     -> Stencil DIM2 Double
+     -> Stencil DIM2 Double
+     -> Array U DIM2 Double
+     -> Array U DIM2 Bool
+     -> Array D DIM2 Double                   
+altMapStencil2 !bd !s1 !s2 !arr !bools
+        -- maps stencil s1 or s2 over arr indices indicated by bools array
+        -- currently does both on all indices and selects after
+        -- this may be ok if laziness + inlining removes redundant computation
+         = alt (mapStencil2 bd s1 arr) (mapStencil2 bd s2 arr)
+           where alt !l !r
+                   = A.szipWith selector (A.szipWith (,) l r) bools
                        where selector (m,n) True  = m
                              selector (m,n) False = n
+
 -- Stencils
 
 leftSt :: Stencil DIM2 Double -- odd rows from b, even from r
@@ -132,6 +137,5 @@ rightSt :: Stencil DIM2 Double -- even rows from b, odd from r
 rightSt =   [stencil2|  0 1 0 
                         0 1 1 
                         0 1 0 |]
-
 
 {-# NOINLINE solveLaplace #-}
