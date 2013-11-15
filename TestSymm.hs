b2316ab608a1ed19895cc9db50038ca333d51a0d
Dominic Steinitz <dominic@steinitz.org>
2013-11-15 11:26:28 +0000

    Test now working.

--- TestSymm.hs
+++ TestSymm.hs
@@ -1,24 +1,38 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleInstances   #-}
 
 module Main (main) where
 
 import Data.Array.Repa
-import SolverOverR
+import RedBlackStencil
 
-dx, dy :: Rational
+import Text.PrettyPrint
+import Text.PrettyPrint.HughesPJClass
+
+instance (Source t a, Pretty a) => Pretty (Array t DIM1 a) where
+ pPrint a = brackets $ hcat $ punctuate (comma <> space) elems
+   where
+     elems = [ pPrint (a!j) | i <- [0..n-1], let j = Z:. i ]
+     Z :. n = extent a
+
+instance (Source t a, Pretty a) => Pretty (Array t DIM2 a) where
+ pPrint a = vcat elems
+   where
+     elems = [ pPrint (slice a j) | i <- [0..n-1], let j = Any :. i :. All]
+     Z :. n :. _m = extent a
+
+dx :: Rational
 dx = 1
-dy = 1 / 2
 
-n, m :: Int
-n = 5
-m = 7
+n :: Int
+n = 7
 
 r, z, u :: Double
 r = 5.0
 z = 0.0
 u = 1.0
 
-arr = fromListUnboxed (Z :. n + 1 :. m + 1) (replicate ((n+1) * (m+1)) z)
+arr = fromListUnboxed (Z :. n + 1 :. n + 1) (replicate ((n+1) * (n+1)) z)
 
 bndVal a = traverse a id f
   where
@@ -33,7 +47,7 @@ bndVal a = traverse a id f
       = r * (1.0 + x * (x - 1.0))
       where x = fromRational $ (fromIntegral ix) * dx
     f get (Z :. ix :. iy)
-      | iy == m
+      | iy == n
       = r * (1.0 + x * (1.0 - x))
       where x = fromRational $ (fromIntegral ix) * dx
     f get (Z :. ix :. iy)
@@ -54,7 +68,7 @@ bndMask a = traverse a id f
       = z
       where x = fromRational $ (fromIntegral ix) * dx
     f get (Z :. ix :. iy)
-      | iy == m
+      | iy == n
       = z
       where x = fromRational $ (fromIntegral ix) * dx
     f get (Z :. ix :. iy)
@@ -73,7 +87,7 @@ checkConvergence dx dy a = traverse a id f
       | iy == 0
       = get (Z :. ix :. iy)
     f get (Z :. ix :. iy)
-      | iy == m
+      | iy == n
       = get (Z :. ix :. iy)
     f get (Z :. ix :. iy)
       = abs (v - u / denom)
@@ -87,9 +101,10 @@ checkConvergence dx dy a = traverse a id f
 main = do
   p <- computeP $ bndMask arr
   v <- computeP $ bndVal arr
-  t <- solveLaplace 91 1.0 0.5 1.0 p v arr
-  xs :: [Array U DIM1 Double] <- mapM (\i -> computeP $ slice t (Any :. i)) [0..m]
-  mapM_ (putStrLn . show) xs
-  c :: Array U DIM2 Double <- computeP $ checkConvergence (fromRational dx) (fromRational dy) t
-  ys :: [Array U DIM1 Double] <- mapM (\i -> computeP $ slice c (Any :. i)) [0..m]
-  mapM_ (putStrLn . show) ys
+  t <- solveLaplace 91 1.0 p v arr
+  putStrLn "\nResults\n"
+  putStrLn $ render $ pPrint t
+  c :: Array U DIM2 Double <- computeP $ checkConvergence (fromRational dx) (fromRational dx) t
+  putStrLn "\nConvergence\n"
+  putStrLn $ render $ pPrint c
+
