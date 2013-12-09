86b1d830c8bf21b25f834efa63cf5ad717c14618
Dominic Steinitz <dominic@steinitz.org>
2013-12-09 08:44:58 +0000

    Correct spelling mistake and remove unnecessary whitespace.

--- RedBlackStencil.hs
+++ RedBlackStencil.hs
@@ -12,11 +12,11 @@
 
 module RedBlackStencil
    (solveLaplace)
-where	
+where
 import Data.Array.Repa                 as A
 import Data.Array.Repa.Stencil         as A
 import Data.Array.Repa.Stencil.Dim2    as A
- 
+
 
 -- | Solver for the Laplace equation.
 solveLaplace :: Monad m
@@ -36,10 +36,10 @@ solveLaplace !steps !omega !arrBoundMask !arrBoundValue !arrInit
        redInit         <- computeP $ projectRed arrInit
        blackInit       <- computeP $ projectBlack arrInit
        let go 0 !r !b = computeP $ combineRB r b -- return final combined array
-           go n !r !b 
+           go n !r !b
             = do (r',b') <- relaxLaplace r b
                  go (n - 1) r' b'
-                  
+
            {-# INLINE relaxLaplace #-}
            relaxLaplace !r !b
             = do
@@ -56,14 +56,14 @@ solveLaplace !steps !omega !arrBoundMask !arrBoundValue !arrInit
                        $ A.smap (/4)
                        $ altMapStencil2 (BoundConst 0) rightSt leftSt r'
                        -- Note use of r' to compute b'
-                 return (r' , b')     
+                 return (r' , b')
        go steps redInit blackInit
- 
+
 relaxWith :: Double -> Double -> Double -> Double
 relaxWith omega old new = (1-omega)*old + omega*new
 
 combineRB :: Array U DIM2 Double -> Array U DIM2 Double -> Array D DIM2 Double
-combineRB r b =     -- arr(i,j) 
+combineRB r b =     -- arr(i,j)
                     --     | even(i+j) = r(i, j `div` 2)
                     --     | otherwise = b(i, j `div` 2)
                     -- arr has i <- 0..n-1 , j <- 0..2m-1
@@ -71,31 +71,31 @@ combineRB r b =     -- arr(i,j)
                     -- b   has i <- 0..n-1 , j <- 0..m-1
             traverse2 r b
                      (\ (e :. i :. j) _ -> (e :. i :. 2*j))
-                     (\ get1 get2 (e :. i :. j) -> 
+                     (\ get1 get2 (e :. i :. j) ->
                               (if even(i+j) then get1 else get2)
                                 (e :. i :. j `div` 2))
-                                
+
 {-# INLINE combineRB #-}
 
 projectRed :: Array U DIM2 Double -> Array D DIM2 Double
-projectRed arr =  
+projectRed arr =
     -- Expects even number of columns for arr
                      -- r(i,j) = arr(i, 2*j + (i `mod` 2))
                      -- arr has i <- 0..n-1, j <- 0..2m-1
                      -- r   has i <- 0..n-1 , j <- 0..m-1
-              traverse arr 
+              traverse arr
                       (\ (e :. i :. j) -> (e :. i :. (j `div` 2)))
                       (\get (e :. i :. j) -> get (e :. i :. 2*j + (i `mod` 2)))
 
 {-# INLINE projectRed #-}
 
 projectBlack :: Array U DIM2 Double -> Array D DIM2 Double
-projecctBlack arr =  
+projectBlack arr =
     -- Expects even number of columns for arr
                      -- b(i,j) = arr(i, 2*j + ((i+1) `mod` 2))
                      -- arr has i <- 0..n-1, j <- 0..2m-1
                      -- b   has i <- 0..n-1 , j <- 0..m-1
-             traverse arr 
+             traverse arr
                       (\ (e :. i :. j) -> (e :. i :. (j `div` 2)))
                       (\get (e :. i :. j) -> get (e :. i :. 2*j + ((i+1) `mod` 2)))
 
@@ -106,29 +106,29 @@ altMapStencil2
       -> Stencil DIM2 Double
       -> Stencil DIM2 Double
       -> Array U DIM2 Double
-      -> Array D DIM2 Double                   
+      -> Array D DIM2 Double
 altMapStencil2 !bd !s1 !s2 !arr
         -- Maps stencil s1 on even rows and s2 on odd rows of arr.
         -- Currently does both on all indices and selects after.
         -- This may be ok if laziness + inlining removes redundant computation
          = traverse2 (mapStencil2 bd s1 arr) (mapStencil2 bd s2 arr)
                      (\ e _ -> e)
-                     (\ get1 get2 (e :. i :. j) -> 
+                     (\ get1 get2 (e :. i :. j) ->
                             (if even i then get1 else get2) (e :. i :. j)
                      )
-                     
+
 {-# INLINE altMapStencil2 #-}
 
 -- Stencils
 
 leftSt :: Stencil DIM2 Double -- odd rows from b, even from r
-leftSt  =   [stencil2|  0 1 0         
-                        1 1 0 
+leftSt  =   [stencil2|  0 1 0
+                        1 1 0
                         0 1 0 |]
 
 rightSt :: Stencil DIM2 Double -- even rows from b, odd from r
-rightSt =   [stencil2|  0 1 0 
-                        0 1 1 
+rightSt =   [stencil2|  0 1 0
+                        0 1 1
                         0 1 0 |]
 
 {-# NOINLINE solveLaplace #-}
